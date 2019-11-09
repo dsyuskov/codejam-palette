1f3556d75ef6efce5f448356f1a625dc7760c0fd
int <yuskovss@gmail.com>
2019-11-09 22:51:06 +0400

    feat: add choose-color

--- codejam-palette/src/app/index.js
+++ codejam-palette/src/app/index.js
@@ -1,14 +1,18 @@
 import '../scss/main.scss';
+
 let canvasWidth = 512;
 let canvasHeight = 512;
-let pixelSize = 32;
-
+let pixelCount = 16;
+let pixelSize = canvasWidth / pixelCount;
+let image = [];
 //let currentTool = localStorage.getItem('currentTool') ? localStorage.getItem('currentTool') : 'pensil';
 let currentTool = 'pensil';
 let unSelectTools = unSelectPensil;
 
-let currentColor = 'blue';
-let prevColor = 'green';
+let currentColor = localStorage.getItem('currentColor') ? localStorage.getItem('currentColor') : '#000000';
+let prevColor = localStorage.getItem('prevColor') ? localStorage.getItem('prevColor') : '#ffffff';
+let currentColorElement;
+let prevColorElement;
 
 let isDrawing = false;
 
@@ -17,9 +21,10 @@ const canvas = document.getElementById("canvas");
   canvas.height = canvasHeight;
 
 const ctx = canvas.getContext('2d');  
+canvasClear();
 
 //--------tools-------------
-function selectTool(item) {    
+function selectTool(item) {
   document.querySelector(`.tools__item--${currentTool}`).classList.remove('selected');
   item.classList.add('selected');
   
@@ -34,8 +39,8 @@ function selectTool(item) {
       break;
     }
     case 'choose-color': {      
-      selectPaintBucket();
-      unSelectTools = unSelectPaintBucket; 
+      selectChooseColor();
+      unSelectTools = unSelectChooseColor; 
       break;
     }
     case 'pensil': {      
@@ -44,13 +49,13 @@ function selectTool(item) {
       break;
     }
     case 'move': {      
-      selectPaintBucket();
-      unSelectTools = unSelectPaintBucket;
+      selectMove();
+      unSelectTools = unSelectMove;
       break;
     }
     case 'transform': {      
-      selectPensil();
-      unSelectTools = unSelectPensil; 
+      selectTransform();
+      unSelectTools = unSelectTransform; 
       break;
     }
     default: {
@@ -59,17 +64,64 @@ function selectTool(item) {
     }
   }  
 }
- 
-document.querySelectorAll('.tools__item').forEach((item) => { 
+
+document.querySelectorAll('.tools__item').forEach((item) => {   
     item.addEventListener('click', () => selectTool(item)); 
     if (item.dataset.type === currentTool) {
       item.classList.add('selected');
       selectPensil();
     }
 });
+//--------colors------------
+const currentColorInput = document.getElementById('current-color');
+currentColorInput.addEventListener('change', (item) => {
+  changeColor(item.target.value)
+});
 
-let lastX = 0;
-let lastY = 0;
+function selectColor(item) {  
+  if (item.firstChild !== null) {
+    switch(item.dataset.color) {
+      case 'current': {                
+        currentColorInput.click();
+        currentColorElement.style.backgroundColor = currentColor;   
+        break;
+      }
+      case 'prev': {        
+        let cur = currentColor;
+        currentColor = prevColor;
+        prevColor = cur;
+        currentColorElement.style.backgroundColor = currentColor;
+        prevColorElement.style.backgroundColor = prevColor;
+        break;
+      }
+      default: {
+        changeColor(item.dataset.color);        
+      }
+    }
+  }
+}
+
+document.querySelectorAll('.colors__item').forEach((item) => {   
+  item.addEventListener('click', () => selectColor(item));
+  
+  if (item.firstChild !== null) {
+    switch(item.dataset.color) {
+      case 'current': {                
+        currentColorElement = item.firstChild;        
+        item.firstChild.style.backgroundColor = currentColor;
+        break;
+      }
+      case 'prev': {        
+        prevColorElement = item.firstChild;
+        item.firstChild.style.backgroundColor = prevColor;
+        break;
+      }
+      default: {
+        item.firstChild.style.backgroundColor = item.dataset.color;
+      }
+    }
+  }
+});
 
 function selectPaintBucket() {
   console.log('paint-bucket');
@@ -79,8 +131,13 @@ function unSelectPaintBucket() {
   console.log('paint-bucket');
 }
 
-function selectChooseColor(){}
-function unSelectChooseColor(){}
+function selectChooseColor(){
+  canvas.addEventListener('mousedown', chooseColor);  
+}
+
+function unSelectChooseColor(){
+  canvas.removeEventListener('mousedown', chooseColor);
+}
 
 function selectPensil() {    
   canvas.addEventListener('mousedown', startPensil);  
@@ -95,30 +152,49 @@ function unSelectPensil() {
   canvas.removeEventListener('mouseout', stopPensil);
 }
 
-function selectMove(){}
+function selectMove(){
+  save();
+}
 function unSelectMove(){}
 
-function selectTransform(){}
+function selectTransform(){  
+}
 function unSelectTransform(){}
+//-----------chooce-color----------
+function chooseColor(event){
+  let {x, y} = getPos(event);
+  changeColor( image[x][y]);  
+}
+function changeColor(color) {  
+  if (currentColor !== color) {
+    prevColor = currentColor;
+    currentColor = color;
+    localStorage.setItem('currentColor', currentColor);
+    localStorage.setItem('prevColor', prevColor);
+    currentColorElement.style.backgroundColor = currentColor;
+    prevColorElement.style.backgroundColor = prevColor;    
+  }  
+}
 
+//------------pensil----------
+let lastX = 0;
+let lastY = 0;
 
 function startPensil(event) {
-  let startX = Math.floor(event.offsetX / pixelSize) * pixelSize;
-  let startY = Math.floor(event.offsetY / pixelSize) * pixelSize;
-  drawPixel(ctx, startX, startY, pixelSize, currentColor);
-  lastX = startX;
-  lastY = startY;
+  let {x, y} = getPos(event);
+  drawPixel(ctx, x, y, pixelSize, currentColor);
+  lastX = x;
+  lastY = y;
   isDrawing = true;
 }
 
 function drawPensil(event) {
-  let startX = Math.floor(event.offsetX / pixelSize) * pixelSize;
-  let startY = Math.floor(event.offsetY / pixelSize) * pixelSize;
+  let {x, y} = getPos(event);
   if (isDrawing) {
-    drawLine(lastX, lastY, startX, startY);
+    drawLine(lastX, lastY, x,y);
   }  
-  lastX = startX;
-  lastY = startY;   
+  lastX = x;
+  lastY = y;     
 }
 
 function stopPensil() {
@@ -127,10 +203,21 @@ function stopPensil() {
 
 
 
+function save() {  
+  canvasClear(pixelCount);  
+}
 
+function canvasClear() {
+  for (let i = 0; i <= pixelCount; i++){
+    image[i] = [];
+    for (let j = 0; j <= pixelCount; j++){
+      image[i][j] = '#ffffff';
+      drawPixel(ctx, i, j, pixelSize, '#ffffff')
+    }
+  }
+}
 
-
-
+save();
 
 document.querySelectorAll('.panel__item').forEach(item => {
     item.addEventListener('click', () => {
@@ -160,39 +247,38 @@ const hex2rgba = (hex, alpha = 1) => {
     return `rgba(${r},${g},${b},${alpha})`;
 };
 
-function drawPixel(canvas, startX, startY, sizePixel, color) {
+function getPos(event) {
+  let x = Math.floor(event.offsetX / pixelSize);
+  let y = Math.floor(event.offsetY / pixelSize);
+  return {x, y};
+}
+
+function drawPixel(canvas, x, y, pixelSize, color) {  
+  image[x][y] = color;
   canvas.fillStyle = color;
-  canvas.fillRect(startX, startY, sizePixel, sizePixel);
+  canvas.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
 }
 
 function drawLine(x1, y1, x2, y2) {
+  let deltaX = Math.abs(x2 - x1);
+  let deltaY = Math.abs(y2 - y1);
+  let signX = x1 < x2 ? 1 : -1;
+  let signY = y1 < y2 ? 1 : -1;
+  let error = deltaX - deltaY;
   
-  var deltaX = Math.abs(x2 - x1);
-  var deltaY = Math.abs(y2 - y1);
-  var signX = x1 < x2 ? 1 : -1;
-  var signY = y1 < y2 ? 1 : -1;
-  //
-  var error = deltaX - deltaY;
-  //
-  drawPixel(ctx, x2, y2, pixelSize, currentColor);
-  while(x1 != x2 || y1 != y2) 
- {      
-      drawPixel(ctx, x1, y1, pixelSize, currentColor);
-      var error2 = error * 2;
-      //
-      if(error2 > -deltaY) 
-      {
-          error -= deltaY;
-          x1 += signX;
-      }
-      if(error2 < deltaX) 
-      {
-          error += deltaX;
-          y1 += signY;
-      }
+  while(x1 != x2 || y1 != y2) {      
+    drawPixel(ctx, x1, y1, pixelSize, currentColor);
+    let error2 = error * 2;
+    if(error2 > -deltaY) {
+      error -= deltaY;
+      x1 += signX;
+    }
+    if(error2 < deltaX) {
+      error += deltaX;
+      y1 += signY;
+    }
   }
 }
-
 
 function drawImage(canvas, dataArray) {
     let sizePixel = 0;
